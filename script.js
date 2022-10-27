8bbd3c9963ed11cee7ff45eecad9c06b93cc1e3d
Agustin Lorenzi <agustinlorenzi6@gmail.com>
2022-10-27 15:28:07 -0300

    Simplificar y ordenar funciones

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //Datos. Codigo, descripcion y precios de los productos.
 
-//Funciones.
+//Clase constructora de los objetos articulos
 class Articulo {
     constructor(codigo, descripcion, precio) {
         this.codigo = Number(codigo);
@@ -10,11 +10,10 @@ class Articulo {
     }
     mostrarEleccion(cantidad, descripcion, precio) {
         alert("Usted compro " + cantidad + " " + descripcion + " de $" + precio + " c/u. \nEste articulo representa en su cuenta $" + subtotal + ".")
-
     }
 }
 
-//objetos
+//datos de los objetos articulos
 const articulo1 = new Articulo(1, "Crema Hidratante para Manos", 500);
 const articulo2 = new Articulo(2, "Oro Liquido sin Formol", 350);
 const articulo3 = new Articulo(3, "Tratamiento a base de aceites 80 ml", 475);
@@ -28,62 +27,69 @@ const articulo10 = new Articulo(10, "Fragancia Natural", 253);
 const articulo11 = new Articulo(11, "Mascarilla Esfoliante Facial", 899);
 const articulo12 = new Articulo(12, "Exfoliante de Labios", 423);
 
+//array que contiene todo el listado de articulos
 const articulos = [articulo1, articulo2, articulo3, articulo4, articulo5, articulo6, articulo7, articulo8, articulo9, articulo10, articulo11, articulo12]
 
+//Arma el listado del carrito
+const listadoCarrito = [];
+let comprado = ";"
+function detalleCarro(codigo, descripcion, cantidad, precio) {
+    comprado = "Codigo: " + codigo + ". " + "Descripción: " + descripcion + ". Precio: $" + precio + ". Cantidad: " + cantidad;
+    listadoCarrito.push(comprado)
+}
+
+//Mostrar antes del pago como va el carro
+const guardado1 = [];
+let guardar1 = "";
+function muestraIntermediaCarro() {
+    for (const articulo of listadoCarrito) {
+        guardar1 = articulo
+        guardado1.push(guardar1)
+    }
+    alert("Los articulos que compraste son: \n" + guardado1.join("\n") + "\nY el total represento: $" + total)
+}
 
 //Suma el monto total de la compra.
 let total = 0;
 let subtotal = 0;
-let listadoCarrito = []
-let comprado = ""
-function totalCarrito(codigo, descripcion, precio, cantidad) {
+function totalCarrito(precio, cantidad) {
     subtotal = precio * cantidad;
     total = total + subtotal;
-    comprado = "codigo: " + codigo + ". " + descripcion + " $" + precio + ". Cantidad:" + cantidad;
-    listadoCarrito.push(comprado)
 }
 
 //Calcula el monto a pagar segun la forma de pago, teniendo en cuenta los intereses.
 let interes = 0;
 let cuotas = 0;
 function formaPago() {
-    if (total > 0) {
-        alert("Su compra fue de $" + total);
-        let pagos = prompt
-            ("¿En cuantos pagos desea abonar esta compra?.\n Siendo el interes: \n-En un pago sin interes.\n-En 3 pagos 10%.\n-En 6 pagos 15%.\n-En 12 pagos 20%.");
-        switch (pagos) {
-            case "1":
-                cuotas = 1;
-                interes = 0;
-                alert("Usted va a pagar $" + total);
-                solicitarDatos();
-                break;
-            case "3":
-                cuotas = 3;
-                interes = total * 0.10;
-                alert("Usted va a pagar $" + (total + interes));
-                solicitarDatos();
-                break;
-            case "6":
-                cuotas = 6;
-                interes = total * 0.15;
-                alert("Usted va a pagar $" + (total + interes));
-                solicitarDatos();
-                break;
-            case "12":
-                cuotas = 12;
-                interes = total * 0.20;
-                alert("Usted va a pagar $" + (total + interes));
-                solicitarDatos();
-                break;
-            default:
-                alert("No es posible esa forma de pago, por favor indique una correcta.")
-                formaPago();
-                break;
-        }
-    } else {
-        alert("Lo invitamos a ver nuestro catalogo, quiza encuentre algo de su interes.");
+    let pagos = prompt
+        ("¿En cuantos pagos desea abonar esta compra?.\n Siendo el interes: \n-En un pago sin interes.\n-En 3 pagos 10%.\n-En 6 pagos 15%.\n-En 12 pagos 20%.");
+    switch (pagos) {
+        case "1":
+            cuotas = 1;
+            interes = 0;
+            alert("Usted va a pagar $" + total);
+            break;
+        case "3":
+            cuotas = 3;
+            interes = total * 0.10;
+            alert("Usted va a pagar $" + (total + interes));
+            break;
+        case "6":
+            cuotas = 6;
+            interes = total * 0.15;
+            alert("Usted va a pagar $" + (total + interes));
+            break;
+        case "12":
+            cuotas = 12;
+            interes = total * 0.20;
+            alert("Usted va a pagar $" + (total + interes));
+            break;
+        default:
+            alert("No es posible esa forma de pago, por favor indique una correcta.")
+            formaPago();
+            break;
     }
+    solicitarDatos();
 }
 
 //Solicita los datos para enviar su compra al consumidor.
@@ -98,21 +104,12 @@ function solicitarDatos() {
 }
 
 //Muestra al usuario un resumen de su compra.
-const guardado1 = [];
-let guardar1 = "";
+
 function mostrarCompra(nombre, telefono, direccion) {
     if (nombre != "" && telefono != "" && direccion != "") {
-        for (const articulo of listadoCarrito) {
-            guardar1 = articulo
-            guardado1.push(guardar1)
-        }
-        alert("Los articulos que compraste son: \n" + guardado1.join("\n") + "\n" + nombre + ".\nTelefono de Contacto " + telefono + ".\nCompraste productos capilares por un monto de $" + (total) + "\nLo pagaste en " + cuotas +
+        alert("Los articulos que compraste son: \n" + guardado1.join("\n") + "\n" + "Cliente: " + nombre + ".\nTelefono de Contacto " + telefono + ".\nCompraste productos capilares por un monto de $" + (total) + "\nLo pagaste en " + cuotas +
             " pago/s con un interes de $" + interes + "\nY el monto final es de $" + (total + interes) + "\nEnviaremos el pedido a "
             + direccion);
-
-        /*alert(nombre + ".\nTelefono de Contacto " + telefono + ".\nCompraste productos capilares por un monto de $" + (total) + "\nLo pagaste en " + cuotas +
-            " pago/s con un interes de $" + interes + "\nY el monto final es de $" + (total + interes) + "\nEnviaremos el pedido a "
-            + direccion);*/
     } else {
         alert("Complete correctamente todos los datos para que podamos enviarle el pedido.")
         solicitarDatos()
@@ -121,77 +118,102 @@ function mostrarCompra(nombre, telefono, direccion) {
 
 
 
-//Bloque principal. Permite al usuario seleccionar el articulo de su interes.
+//Funcion de Compra
 
-let pregunta = prompt("Desea cargar algun articulo al carrito? si/no")
+function compra (){
+    while (pregunta == "si") {
+        let guardado = []
+        let guardar = ""
+        for (const articulo of articulos) {
+            guardar = articulo.codigo + " " + articulo.descripcion + " $" + articulo.precio
+            guardado.push(guardar)
+        }
+        alert("Lista de Articulos y codigos(utilice el codigo del principio para completar el carrito):\n" + guardado.join("\n"))
 
-while (pregunta == "si") {
-
-    let guardado = []
-    let guardar = ""
-    for (const articulo of articulos) {
-        guardar = articulo.codigo + " " + articulo.descripcion + " $" + articulo.precio
-        guardado.push(guardar)
+        let opcion = prompt("¿Que articulo necesita?, coloque su codigo");
+        let cantidad = parseInt(prompt("¿Que cantidad deseas de este producto?"))
+        if (opcion !== "" && opcion !== 0 && cantidad !== "" && cantidad !== 0) {
+            switch (opcion) {
+                case "1":
+                    totalCarrito(articulo1.precio, cantidad);
+                    detalleCarro(articulo1.codigo, articulo1.descripcion, cantidad, articulo1.precio);
+                    articulo1.mostrarEleccion(cantidad, articulo1.descripcion, articulo1.precio);
+                    break
+                case "2":
+                    totalCarrito(articulo2.codigo, articulo2.descripcion, articulo2.precio, cantidad);
+                    articulo2.mostrarEleccion(cantidad, articulo2.descripcion, articulo2.precio);
+                    break
+                case "3":
+                    totalCarrito(475, cantidad);
+                    break
+                case "4":
+                    totalCarrito(1050, cantidad);
+                    break
+                case "5":
+                    totalCarrito(735, cantidad);
+                    break
+                case "6":
+                    totalCarrito(1528, cantidad);
+                    break
+                case "7":
+                    totalCarrito(875, cantidad);
+                    break
+                case "8":
+                    totalCarrito(983, cantidad);
+                    break
+                case "9":
+                    totalCarrito(759, cantidad);
+                    break
+                case "10":
+                    totalCarrito(253, cantidad);
+                    break
+                case "11":
+                    totalCarrito(899, cantidad);
+                    break
+                case "12":
+                    totalCarrito(423, cantidad);
+                    break
+                default:
+                    alert("El codigo del articulo no es correcto.")
+                    break
+            }
+            pregunta = prompt("¿Desea cargar otro articulo? si/no")            
+        } else {
+            alert("Cargue algun producto o cantidad.")
+        }
     }
-    alert("Lista de Articulos y codigos(utilice el codigo del principio para completar el carrito): \n" + guardado.join("\n"))
-
-    /*alert("Lista de Articulos: " + "\n" + (articulo1.codigo + " - " + articulo1.descripcion + " $" + articulo1.precio)+ "\n" + articulo2 + "\n" + articulo3 + "\n" + articulo4 +
-        "\n" + articulo5 + "\n" + articulo6 + "\n" + articulo7 + "\n" + articulo8 + "\n" + articulo9 + "\n" + articulo10 +
-        "\n" + articulo11 + "\n" + articulo12 + "\n" +
-        "Complete en la siguiente pantalla el codigo del articulo de su eleccion.")*/
-
-    let opcion = prompt("¿Que articulo necesita?, coloque su codigo");
-    let cantidad = Number(prompt("¿Que cantidad deseas de este producto?"))
-    //let descripcion = "";
-    //let precio = "";
-    switch (opcion) {
-        case "1":
-            totalCarrito(articulo1.codigo, articulo1.descripcion, articulo1.precio, cantidad);
-            articulo1.mostrarEleccion(cantidad, articulo1.descripcion, articulo1.precio);
-            break
-        case "2":
-            totalCarrito(articulo2.codigo, articulo2.descripcion, articulo2.precio, cantidad);
-            articulo2.mostrarEleccion(cantidad, articulo2.descripcion, articulo2.precio);
-            break
-        case "3":
-            totalCarrito(475, cantidad);
-            break
-        case "4":
-            totalCarrito(1050, cantidad);
-            break
-        case "5":
-            totalCarrito(735, cantidad);
-            break
-        case "6":
-            totalCarrito(1528, cantidad);
-            break
-        case "7":
-            totalCarrito(875, cantidad);
-            break
-        case "8":
-            totalCarrito(983, cantidad);
-            break
-        case "9":
-            totalCarrito(759, cantidad);
-            break
-        case "10":
-            totalCarrito(253, cantidad);
-            break
-        case "11":
-            totalCarrito(899, cantidad);
-            break
-        case "12":
-            totalCarrito(423, cantidad);
-            break
-        default:
-            alert("El codigo del articulo no es correcto.")
-            break
-    }
-    pregunta = prompt("¿Desea cargar otro articulo? si/no")
 }
 
-//Llamado a la funcion forma de pago.
-formaPago();
+/*let cant = 0;
+let elim = "";
+function eliminar(){
+    let articuloEliminar =  listadoCarrito.includes(prompt("¿Que articulo desea eliminar de la lista?")).toLowerCase();
+    for (const objeto of listadoCarrito){
+        if (objeto.descripcion == articuloEliminar){        
+            elim = array.indexOf(objeto)       
+        }      
+    }
+    array.splice(elim,1)
+    muestraIntermediaCarro()
+}*/
+
+
+
+//Bloque principal. Permite al usuario seleccionar el articulo de su interes.
+
+let pregunta = (prompt("Desea cargar algun articulo al carrito? si/no")).toLowerCase();
+
+if (pregunta === "si") {
+    compra();
+    muestraIntermediaCarro();    
+    formaPago();
+} else {
+    alert("Lo invitamos a ver nuestro catalogo, quiza encuentre algo de su interes.");
+}
+
+
+
+
 
 
 
