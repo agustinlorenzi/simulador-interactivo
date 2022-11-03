33c4a9138b71f27116a4d7952a1e924d080dc134
Agustin Lorenzi <agustinlorenzi6@gmail.com>
2022-11-03 16:41:47 -0300

    Pulido de funcionamiento y entrega

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-//Datos. Codigo, descripcion y precios de los productos.
+///////////////////////////////////////Datos. Codigo, descripcion y precios de los productos.
 
 //Clase constructora de los objetos articulos
 class Articulo {
@@ -29,15 +29,17 @@ const articulos = [articulo1, articulo2, articulo3, articulo4, articulo5, articu
 
 
 
-//funcion eleccion, donde el usuario puede elegir si anexa articulos, cambiar cantidades, eliminar items o 
+
+
+///////////////funcion eleccion, donde el usuario puede elegir si anexa articulos, cambiar cantidades, eliminar items o 
 //confirmar la compra.
 function eleccion() {
-    let elegir = prompt("Elija como proceder como proceder con la compra:\n1-Agregar articulos.\n2-Modificar cantidad.\n3-Eliminar Articulo.\n4-Confirmar carro.")
+    let elegir = prompt("Elija como proceder con la compra:\n1-Agregar articulos.\n2-Modificar cantidad.\n3-Eliminar Articulo.\n4-Confirmar carro.")
     if (elegir !== "") {
         switch (elegir) {
             case "1":
-                pregunta = "si"
-                listaPrecio();
+                pregunta = "si";
+                listaPrecio(pregunta);
                 break;
             case "2":
                 modificarCantidad();
@@ -46,7 +48,12 @@ function eleccion() {
                 eliminar();
                 break;
             case "4":
-                formaPago();
+                if (comprados.length !== 0) {
+                    formaPago();
+                } else {
+                    alert("El carrito esta vacio.")
+                    principio()
+                }
             default:
                 break;
         }
@@ -58,202 +65,222 @@ function eleccion() {
 
 
 
-    //Funciones Generales.
-    //Mostrar carro antes del pago
-    let guardar1 = "";
-       
-    function muestraIntermediaCarro() {
-        let guardado1 = []; 
+
+/////////////////////////////////////////////////////Funciones Generales.
+//Mostrar carro antes del pago.
+let guardar1 = "";
+function muestraIntermediaCarro() {
+    let guardado1 = [];
+    for (const articulo of comprados) {
+        guardar1 = "Codigo: " + articulo.codigo + ". Descripcion: " + articulo.descripcion + ". $" + articulo.precio + ". Cant: " + articulo.cant + ". Subtotal: " + articulo.subtotal;
+        guardado1.push(guardar1)
+    }
+    totalCarrito();
+    alert("Los articulos que compraste son: \n" + guardado1.join("\n") + "\nY el total represento: $" + total)
+    eleccion();
+}
+
+//subtotal.
+let subtotal = 0;
+function subTotal(precio, cant) {
+    subtotal = 0;
+    subtotal = precio * cant;
+}
+
+//Suma el monto total de la compra.
+let total = 0;
+function totalCarrito() {
+    total = 0;
+    for (const articulo of comprados) {
+        total = total + articulo.precio * articulo.cant;
+    }
+}
+
+//Calcula el monto a pagar segun la forma de pago, teniendo en cuenta los intereses.
+let interes = 0;
+let cuotas = 0;
+function formaPago() {
+    let pagos = prompt
+        ("¿En cuantos pagos desea abonar esta compra?.\n Siendo el interes: \n-En un pago sin interes.\n-En 3 pagos 10%.\n-En 6 pagos 15%.\n-En 12 pagos 20%.");
+    switch (pagos) {
+        case "1":
+            cuotas = 1;
+            interes = 0;
+            alert("Usted va a pagar $" + total);
+            break;
+        case "3":
+            cuotas = 3;
+            interes = total * 0.10;
+            alert("Usted va a pagar $" + (total + interes));
+            break;
+        case "6":
+            cuotas = 6;
+            interes = total * 0.15;
+            alert("Usted va a pagar $" + (total + interes));
+            break;
+        case "12":
+            cuotas = 12;
+            interes = total * 0.20;
+            alert("Usted va a pagar $" + (total + interes));
+            break;
+        default:
+            alert("No es posible esa forma de pago, por favor indique una correcta.")
+            formaPago();
+            break;
+    }
+    solicitarDatos();
+}
+
+//Solicita los datos para enviar su compra al consumidor.
+function solicitarDatos() {
+    alert("Complete los siguientes datos para que le enviemos su pedido.");
+    let nombre = prompt("Coloque su nombre completo: ");
+    let telefono = Number(prompt("Coloque su numero telefonico: "));
+    let direccion = prompt("Coloque su direccion: ");
+    /*aca se llama a la funcion mostrarCompra, que muestra un resumen de la misma, pasando las
+    variables locales para que la otra funcion pueda usarlas*/
+    mostrarCompra(nombre, telefono, direccion);
+}
+
+//Muestra al usuario un resumen de su compra.
+const guardado2 = []
+let guard = "";
+function mostrarCompra(nombre, telefono, direccion) {
+    if (nombre !== "" && telefono !== "" && direccion !== "") {
         for (const articulo of comprados) {
-            guardar1 = "Codigo: " + articulo.codigo + ". Descripcion: " + articulo.descripcion + ". $" + articulo.precio + ". Cant: " + articulo.cant + ". Subtotal: " + articulo.subtotal;
-            guardado1.push(guardar1)
+            guard = "Codigo: " + articulo.codigo + ". Descripcion: " + articulo.descripcion + ". $" + articulo.precio + ". Cant: " + articulo.cant + ". Subtotal: " + articulo.subtotal;
+            guardado2.push(guard);
         }
-        totalCarrito();
-        alert("Los articulos que compraste son: \n" + guardado1.join("\n") + "\nY el total represento: $" + total)
-        eleccion();
+        alert("Los articulos que compraste son: \n" + guardado2.join("\n") + "\n" + "Cliente: " + nombre + ".\nTelefono de Contacto " + telefono + ".\nCompraste productos capilares por un monto de $" + total + "\nLo pagaste en " + cuotas +
+            " pago/s con un interes de $" + interes + "\nY el monto final es de $" + (total + interes) + "\nEnviaremos el pedido a "
+            + direccion);
+    } else {
+        alert("Complete correctamente todos los datos para que podamos enviarle el pedido.")
+        solicitarDatos()
     }
+}
 
-    //subtotal.
-    let subtotal = 0;
-    function subTotal(precio, cant) {
-        subtotal = 0;
-        subtotal = precio * cant;
+
+
+
+////////////////////////////Funciones de Agregado, modificacion, eliminacion o confirmacion de articulos.
+
+// constructor articulos comprados
+class Comprado {
+    constructor(codigo, descripcion, precio, cant, subtotal) {
+        this.codigo = Number(codigo);
+        this.descripcion = descripcion;
+        this.precio = Number(precio);
+        this.cant = Number(cant);
+        this.subtotal = Number(subtotal)
     }
+    mostrarEleccion(cantidad, descripcion, precio, subtotal) {
+        alert("Usted compro " + cantidad + " " + descripcion + " de $" + precio + " c/u. \nEste articulo representa en su cuenta $" + subtotal + ".")
+    }
+}
 
-    //Suma el monto total de la compra.
-    let total = 0;
-    function totalCarrito() {
-        total = 0;
-        for (const articulo of comprados) {
-            total = total + articulo.precio * articulo.cant;
+//funcion de compra, forma el array de los objetos articulos comprados
+const comprados = [];
+function compra(codigo, descripcion, precio, cant, subtotal) {
+    const comprado = new Comprado(codigo, descripcion, precio, cant, subtotal);
+    comprados.push(comprado);
+    comprado.mostrarEleccion(comprado.cant, comprado.descripcion, comprado.precio, comprado.subtotal);
+}
+
+//Funcion para comprar mostrando la lista de precios general de articulos
+function listaPrecio(pregunta) {
+    let opcion = 0;
+    let cantidad = 0;
+    let cant = 0;
+    while (pregunta === "si") {
+        let guardado = []
+        let guardar = ""
+        for (const articulo of articulos) {
+            guardar = articulo.codigo + " " + articulo.descripcion + " $" + articulo.precio
+            guardado.push(guardar)
         }
-    }
-
-    //Calcula el monto a pagar segun la forma de pago, teniendo en cuenta los intereses.
-    let interes = 0;
-    let cuotas = 0;
-    function formaPago() {
-        let pagos = prompt
-            ("¿En cuantos pagos desea abonar esta compra?.\n Siendo el interes: \n-En un pago sin interes.\n-En 3 pagos 10%.\n-En 6 pagos 15%.\n-En 12 pagos 20%.");
-        switch (pagos) {
-            case "1":
-                cuotas = 1;
-                interes = 0;
-                alert("Usted va a pagar $" + total);
-                break;
-            case "3":
-                cuotas = 3;
-                interes = total * 0.10;
-                alert("Usted va a pagar $" + (total + interes));
-                break;
-            case "6":
-                cuotas = 6;
-                interes = total * 0.15;
-                alert("Usted va a pagar $" + (total + interes));
-                break;
-            case "12":
-                cuotas = 12;
-                interes = total * 0.20;
-                alert("Usted va a pagar $" + (total + interes));
-                break;
-            default:
-                alert("No es posible esa forma de pago, por favor indique una correcta.")
-                formaPago();
-                break;
-        }
-        solicitarDatos();
-    }
-
-    //Solicita los datos para enviar su compra al consumidor.
-    function solicitarDatos() {
-        alert("Complete los siguientes datos para que le enviemos su pedido.");
-        let nombre = prompt("Coloque su nombre completo: ");
-        let telefono = Number(prompt("Coloque su numero telefonico: "));
-        let direccion = prompt("Coloque su direccion: ");
-        /*aca se llama a la funcion mostrarCompra, que muestra un resumen de la misma, pasando las
-        variables locales para que la otra funcion pueda usarlas*/
-        mostrarCompra(nombre, telefono, direccion);
-    }
-
-    //Muestra al usuario un resumen de su compra.
-    const guardado2 = []
-    let guard = "";
-    function mostrarCompra(nombre, telefono, direccion) {
-        if (nombre != "" && telefono != "" && direccion != "") {
-            for (const articulo of comprados) {
-                guard = "Codigo: " + articulo.codigo + ". Descripcion: " + articulo.descripcion + ". $" + articulo.precio + ". Cant: " + articulo.cant + ". Subtotal: " + articulo.subtotal;
-                guardado2.push(guard);
-            }
-            alert("Los articulos que compraste son: \n" + guardado2.join("\n") + "\n" + "Cliente: " + nombre + ".\nTelefono de Contacto " + telefono + ".\nCompraste productos capilares por un monto de $" + total + "\nLo pagaste en " + cuotas +
-                " pago/s con un interes de $" + interes + "\nY el monto final es de $" + (total + interes) + "\nEnviaremos el pedido a "
-                + direccion);
+        alert("Lista de Articulos y codigos(utilice el codigo del principio para completar el carrito):\n" + guardado.join("\n"))
+        opcion = Number(prompt("¿Que articulo necesita?, coloque su codigo"));
+        cantidad = Number(prompt("¿Que cantidad deseas de este producto?"));
+        if (opcion !== "" && opcion !== 0 && cantidad !== "" && cantidad !== 0) {
+            cant = parseInt(cantidad);
+            let artSeleccionado = articulos.find((el) => el.codigo === opcion);
+            subTotal(artSeleccionado.precio, cant)
+            compra(artSeleccionado.codigo, artSeleccionado.descripcion, artSeleccionado.precio, cant, subtotal);
+            pregunta = prompt("¿Desea cargar otro articulo? si/no")
         } else {
-            alert("Complete correctamente todos los datos para que podamos enviarle el pedido.")
-            solicitarDatos()
+            alert("Cargue algun producto o cantidad.")
         }
     }
+    muestraIntermediaCarro();
+}
 
-
-    //Funciones de Agregado, modificacion, eliminacion o confirmacion de articulos.
-    // constructor articulos comprados
-    class Comprado {
-        constructor(codigo, descripcion, precio, cant, subtotal) {
-            this.codigo = Number(codigo);
-            this.descripcion = descripcion;
-            this.precio = Number(precio);
-            this.cant = Number(cant);
-            this.subtotal = Number(subtotal)
-        }
-        mostrarEleccion(cantidad, descripcion, precio, subtotal) {
-            alert("Usted compro " + cantidad + " " + descripcion + " de $" + precio + " c/u. \nEste articulo representa en su cuenta $" + subtotal + ".")
-        }
-    }
-
-    //funcion de compra, forma el array de los objetos articulos comprados
-    const comprados = [];
-    function compra(codigo, descripcion, precio, cant, subtotal) {
-        const comprado = new Comprado(codigo, descripcion, precio, cant, subtotal);
-        comprados.push(comprado);
-        comprado.mostrarEleccion(comprado.cant, comprado.descripcion, comprado.precio, comprado.subtotal);
-    }
-
-    //Funcion para comprar mostrando la lista de precios general de articulos
-    function listaPrecio() {
-        let opcion = 0;
-        let cantidad = 0;
-        let cant = 0;
-        while (pregunta == "si") {
-            let guardado = []
-            let guardar = ""
-            for (const articulo of articulos) {
-                guardar = articulo.codigo + " " + articulo.descripcion + " $" + articulo.precio
-                guardado.push(guardar)
-            }
-            alert("Lista de Articulos y codigos(utilice el codigo del principio para completar el carrito):\n" + guardado.join("\n"))
-            opcion = Number(prompt("¿Que articulo necesita?, coloque su codigo"));
-            cantidad = Number(prompt("¿Que cantidad deseas de este producto?"));            
-            if (opcion !== "" && opcion !== 0 && cantidad !== "" && cantidad !== 0) {
-                cant = parseInt(cantidad);
-                let artSeleccionado = articulos.find((el) => el.codigo === opcion);
-                subTotal(artSeleccionado.precio, cant)
-                compra(artSeleccionado.codigo, artSeleccionado.descripcion, artSeleccionado.precio, cant, subtotal);
-                pregunta = prompt("¿Desea cargar otro articulo? si/no")
-            } else {
-                alert("Cargue algun producto o cantidad.")
-            }
-        }
-        muestraIntermediaCarro();
-    }
-
-    //Modificar cantidad de los articulos
-    function modificarCantidad() {
-        let modificar = Number(prompt("¿Que articulo desea modificar su cantidad?: "));
-        const existe = comprados.some(comprado => comprado.codigo === modificar);
-        if (existe !== false) {
-            let nuevaCantidad = parseInt(prompt("¿Que cantidad desea?: "))
+//Modificar cantidad de los articulos
+function modificarCantidad() {
+    let modificar = Number(prompt("¿Que articulo desea modificar su cantidad?: "));
+    const existe = comprados.some(comprado => comprado.codigo === modificar);
+    if (existe !== false) {
+        let nuevaCantidad = Number(prompt("¿Que cantidad desea?: "))
+        nuevaCantidad = parseInt(nuevaCantidad)
+        if (nuevaCantidad !== 0) {
             const artModificar = comprados.find((el) => el.codigo === modificar);
             artModificar.cant = nuevaCantidad;
             subTotal(artModificar.precio, nuevaCantidad)
             artModificar.subtotal = subtotal
             muestraIntermediaCarro();
         } else {
-            alert("Ese articulo no se encuentra en su carro.");
-            eleccion();
+            alert("No puso cantidad correcta, por favor indiquelo de nuevo.")
+            modificarCantidad()
         }
+    } else {
+        alert("Ese articulo no se encuentra en su carro.");
+        eleccion();
     }
+}
 
-    //Funcion para eliminar los articulos del carro
-    let borrar = 0;
-    function eliminar() {
-        borrar = 0;
-        let elim = Number(prompt("¿Que articulo desea eliminar?: "));
-        const existe = comprados.some(comprado => comprado.codigo === elim);
-        if (existe !== false) {
-            for (const objeto of comprados) {
-                if (objeto.codigo === elim) {
-                    borrar = comprados.indexOf(objeto)
-                }
+//Funcion para eliminar los articulos del carro
+let borrar = 0;
+function eliminar() {
+    borrar = 0;
+    let elim = Number(prompt("¿Que articulo desea eliminar?: "));
+    const existe = comprados.some(comprado => comprado.codigo === elim);
+    if (existe !== false) {
+        for (const objeto of comprados) {
+            if (objeto.codigo === elim) {
+                borrar = comprados.indexOf(objeto)
             }
-        } else {
-            alert("Ese articulo no se encuentra en su carro.");
-            
-            eleccion();
         }
-        comprados.splice(borrar, 1)
-        console.log(comprados)
-        muestraIntermediaCarro()
+    } else {
+        alert("Ese articulo no se encuentra en su carro.");
+
+        eleccion();
     }
+    comprados.splice(borrar, 1)
+    console.log(comprados)
+    muestraIntermediaCarro()
+}
 
 
 
-    //Inicio del programa.
+
+
+
+////////////////////////////////////////////Funcion de arranque.    
+function principio() {
     let pregunta = (prompt("Desea cargar algun articulo al carrito? si/no")).toLowerCase();
     if (pregunta === "si") {
-        listaPrecio();
+        listaPrecio(pregunta);
     } else {
         alert("Lo invitamos a ver nuestro catalogo, quiza encuentre algo de su interes.");
     }
+}
 
+
+
+
+
+//////////////////////////////////////////////////Inicio del programa.
+principio();
 
 
 
